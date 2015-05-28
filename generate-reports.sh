09cc11f96651e8fd0bb11acb9d9f667e4f7ba8de
Ingo Fabbri <i.fabbri@newtown.at>
2015-05-28 17:21:41 +0200

    fix paths and script syntax errors

--- generate-reports.sh
+++ generate-reports.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Run system check first to see if Magento folder detected (error messages from magerun are printed to STDERR)
-if bin/n98-magerun.phar sys:check >/dev/null then;
+if [bin/n98-magerun.phar sys:check >/dev/null]; then
     exit
 fi;
 
@@ -9,8 +9,8 @@ echo "Install addons..."
 bin/install-addons.sh
 echo ""
 
-MAGENTO_VERSION=$(n98-magerun sys:info --format=csv | \grep Version | sed s/Version,//)
-if [ ! -d ~/magento-$MAGENTO_VERSION ]; then;
+MAGENTO_VERSION=$(bin/n98-magerun.phar sys:info --format=csv | \grep Version | sed s/Version,//)
+if [ ! -d ~/magento-$MAGENTO_VERSION ]; then
     echo "Download Magento ${MAGENTO_VERSION}..."
     mkdir ~/magento-$MAGENTO_VERSION || exit
     curl http://www.magentocommerce.com/downloads/assets/$MAGENTO_VERSION/magento-$MAGENTO_VERSION.tar.gz | tar -xz -C ~/magento-$MAGENTO_VERSION
@@ -18,11 +18,11 @@ if [ ! -d ~/magento-$MAGENTO_VERSION ]; then;
 fi;
 mkdir -p var/audit || exit
 echo "Generate reports..."
-bin/n98-magerun.phar sys:module:list --format=csv > audit/modules.csv
-php www/downloader/mage.php list-upgrades | sed -r 's/\s+(\w+): (.*) => (.*)/\1;\2;\3/' > audit/module-updates-from-connect.csv
-bin/n98-magerun.phar sys:info --format=csv > audit/sysinfo.csv
-bin/n98-magerun.phar mpmd:codepooloverrides audit/codepooloverrides.html
-bin/n98-magerun.phar mpmd:corehacks ~/magento-$MAGENTO_VERSION/magento audit/corehacks.html
-bin/n98-magerun.phar dev:module:rewrite:list --format=csv > audit/rewrites.csv
-bin/n98-magerun.phar dev:module:rewrite:conflicts --log-junit=audit/rewrite-conflicts.xml
-bin/phpcs.phar --report=csv --report-file=audit/phpcs.csv --ignore=code/core --standard=~/.phpcs/Ecg www/app
+bin/n98-magerun.phar sys:module:list --format=csv > var/audit/modules.csv
+php downloader/mage.php list-upgrades | sed -r 's/\s+(\w+): (.*) => (.*)/\1;\2;\3/' > var/audit/module-updates-from-connect.csv
+bin/n98-magerun.phar sys:info --format=csv > var/audit/sysinfo.csv
+bin/n98-magerun.phar mpmd:codepooloverrides var/audit/codepooloverrides.html
+bin/n98-magerun.phar mpmd:corehacks ~/magento-$MAGENTO_VERSION/magento var/audit/corehacks.html
+bin/n98-magerun.phar dev:module:rewrite:list --format=csv > var/audit/rewrites.csv
+bin/n98-magerun.phar dev:module:rewrite:conflicts --log-junit=var/audit/rewrite-conflicts.xml
+bin/phpcs.phar --report=csv --report-file=var/audit/phpcs.csv --ignore=code/core --standard=~/.phpcs/Ecg app
